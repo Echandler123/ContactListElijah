262c475a862c3c60e7d9cbdf8be24f470244b3e0
elijah <elijah.chndler@menloschool.org>
2023-01-13 12:39:55 -0800

    h

--- src/Person.java
+++ src/Person.java
@@ -8,5 +8,31 @@
  */
 
 public class Person {
+    String firstName;
+    String lastName;
+    String phoneNumber;
+    public person(String firstName, String lastName, int PhoneNumber)
+    {
+        this.firstName  = firstName;
+        this.lastName =  lastName;
+        this.phoneNumber = phoneNumber;
+    }
     // TODO: Complete the Person class
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + lastName + " - " + phoneNumber;
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -8,5 +8,19 @@
  */
 
 public class Student {
+    private int grade;
+    public Student(String firstName, String lastName, int PhoneNumber, int grade)
+    {
+        super(firstName,lastName, PhoneNumber);
+        this.grade = grade;
+    }
+    public int getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " " + grade;
+    }
     // TODO: Modify and complete this class
 }
