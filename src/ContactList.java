823aca2d069e81d2a3024d086b1459d6b2fbebea
elijah <elijah.chndler@menloschool.org>
2023-01-19 19:32:06 -0800

    h

--- src/ContactList.java
+++ src/ContactList.java
@@ -43,18 +43,22 @@ public class ContactList
         // TODO: Complete the addContact method
         Scanner sc = new Scanner(System.in);
         System.out.println("Please select the type of contact to add:");
+        System.out.println("1. Student");
+        System.out.println("2. Teacher");
         int choice = sc.nextInt();
         if(choice == 1)
         {
             System.out.println("Please fill in the following information.");
             System.out.println("First Name:");
-            String firstName= sc.nextLine();
+            sc.nextLine();
+            String firstName = sc.nextLine();
             System.out.println("Last Name:");
-            String lastName = sc.nextLine();;
+            String lastName = sc.nextLine();
             System.out.println("Grade:");
-            int grade = sc.nextInt();;
+            int grade = sc.nextInt();
             System.out.println("Phone Number:");
-            String phoneNumber = sc.nextLine();;
+            sc.nextLine();
+            String phoneNumber = sc.nextLine();
             Student s = new Student(firstName,lastName,phoneNumber,grade);
             contacts.add(s);
         }
@@ -64,11 +68,11 @@ public class ContactList
             System.out.println("First Name:");
             String firstName = sc.nextLine();
             System.out.println("Last Name:");
-            String lastName = sc.nextLine();;
+            String lastName = sc.nextLine();
             System.out.println("subject:");
-            String subject = sc.nextLine();;
+            String subject = sc.nextLine();
             System.out.println("Phone Number:");
-            String phoneNumber = sc.nextLine();;
+            String phoneNumber = sc.nextLine();
             Teacher t = new Teacher(firstName,lastName,phoneNumber, subject);
             contacts.add(t);
         }
@@ -95,11 +99,11 @@ public class ContactList
         if(sortBy == 0)//first name
         {
             int n = contacts.size();
-            for(int pass = 1; pass < n; pass++) {
-                for(int comp = 0; comp < n - pass; comp++){
-                    if (contacts.get(comp).getFirstName().compareTo(contacts.get(comp + 1).getFirstName()) > 0)
-                        contacts.add(comp,contacts.get(comp + 1));
-                    contacts.remove(comp + 2);
+            for(int i=0; i<n-1; i++) {
+                for(int j=0; j<n-i-1; j++){
+                    if (contacts.get(j).getFirstName().compareTo(contacts.get(j+ 1).getFirstName()) > 0)
+                        contacts.add(j,contacts.get(j + 1));
+                        contacts.remove(j + 2);
                 }
             }
         }
@@ -176,7 +180,7 @@ public class ContactList
         // TODO: Complete the listStudents method
         for(int i =0; i < contacts.size(); i++)
         {
-            if (contacts.get(i)instanceof student == true)
+            if (contacts.get(i) instanceof Student)
             {
                 System.out.println(contacts.get(i));
             }
@@ -188,10 +192,48 @@ public class ContactList
      * until the user exits
      */
     public void run() {
+        Scanner sc = new Scanner(System.in);
+        int input = -1;
         System.out.println("Welcome to your Contacts List");
         System.out.println("Please pick from the following menu options");
-        printMenuOptions();
 
+        while(input != 0) {
+            printMenuOptions();
+            input = sc.nextInt();
+            if (input == 1) {
+                this.addContact();
+            }
+            if (input == 2) {
+                sort(1);
+                printContacts();
+            }
+            if (input == 3) {
+                sort(2);
+                printContacts();
+            }
+            if (input == 4) {
+                sort(0);
+                printContacts();
+            }
+            if (input == 5) {
+                listStudents();
+            }
+            if (input == 6) {
+                System.out.println("Enter a name");
+                String name = sc.nextLine();
+                searchByFirstName(name);
+            }
+            if (input == 7) {
+                System.out.println("Enter a last name");
+                String lastname = sc.nextLine();
+                searchByLastName(lastname);
+            }
+            if (input == 8) {
+                System.out.println("Enter a phone number");
+                String phonenumber = sc.nextLine();
+                searchByPhoneNumber(phonenumber);
+            }
+        }
         // TODO: Complete the run method
     }
 
